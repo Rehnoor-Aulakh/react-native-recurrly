a37a78f3b8868c8eec54337d00984619f8b773d4
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2026-09-14 06:56:25 +0530

    Added tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,19 @@
 import "@/global.css";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
+      <Link
+        href="/onboarding"
+        className="bg-primary mt-4 rounded text-white p-4"
+      >
+        Go to onboarding
+      </Link>
     </View>
   );
 }
