9ceb00760a7c1a67290afc00ffb7de68cd29e6b1
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2026-09-14 07:33:21 +0530

    Added the routes

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,6 @@
-import "@/global.css";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
 
-export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link
-        href="/onboarding"
-        className="bg-primary mt-4 rounded text-white p-4"
-      >
-        Go to onboarding
-      </Link>
-    </View>
-  );
+export default function Index() {
+  return <Redirect href="/(tabs)" />;
 }
+
